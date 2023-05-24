e00d71cc2a93ebe98d096f857946891512df5734
datogithub <104518080+datogithub@users.noreply.github.com>
2023-05-24 15:37:11 -0700

    create full responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
         <footer className="footer">
             <Container>
                 <Row>
-                    <Col lg="4">
+                    <Col lg="4" md="6" className="mb-4">
                     <div className="logo">
                         
                         <div>
@@ -25,7 +25,7 @@ const Footer = () => {
                         </p>
                     </Col>
 
-                    <Col lg="3">
+                    <Col lg="3" md="3" className="mb-4">
                         <div className="footer__quick-links">
                             <h4 className="quick__link-title">Top Categories</h4>
                             <ListGroup>
@@ -48,7 +48,7 @@ const Footer = () => {
                         </div>
                     </Col>
 
-                    <Col lg="2">
+                    <Col lg="2" md="3" className="mb-4">
                     <div className="footer__quick-links">
                             <h4 className="quick__link-title">Useful Links</h4>
                             <ListGroup>
@@ -71,7 +71,7 @@ const Footer = () => {
                         </div>
                     </Col>
 
-                    <Col lg="3">
+                    <Col lg="3" md="4">
                     <div className="footer__quick-links">
                             <h4 className="quick__link-title">Contact</h4>
                             <ListGroup className="footer__contact">
