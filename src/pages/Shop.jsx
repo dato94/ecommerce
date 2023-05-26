8a0b39fda6766bc8aaf772772d5dbc63704d2673
datogithub <104518080+datogithub@users.noreply.github.com>
2023-05-25 20:55:02 -0700

    create common section

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+
 const Shop = () => {
     return (
-        <div>Shop</div>
+        <Helmet title="shop">
+            <CommonSection title="Products" />
+        </Helmet>
     )
 }
 
