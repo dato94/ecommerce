d0fa483cb3db2d236fd5d9ace5ac37dce95971ba
datogithub <104518080+datogithub@users.noreply.github.com>
2023-05-26 16:01:29 -0700

    create products review

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,24 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 import { Container, Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import products from "../assets/data/products";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
+import { motion } from "framer-motion";
 
 import "../styles/product-details.css";
 
 
 const ProductDetails = () => {
 
+    const [tab, setTab] = useState("desc")
     const {id} = useParams()
     const product = products.find(item => item.id === id)
 
-    const {imgUrl, productName, price, avgRating, review, description, shortDesc} = product
+    const {imgUrl, productName, price, avgRating, reviews, description, shortDesc} = product
 
     return (
-        <Helmet>
-            <CommonSection />
+        <Helmet title={productName}>
+            <CommonSection title={productName}/>
 
             <section className="pt-0">
                 <Container>
@@ -30,7 +32,7 @@ const ProductDetails = () => {
                         <Col lg="6">
                             <div className="product__details">
                                 <h2>{productName}</h2>
-                                <div className="product__rating">
+                                <div className="product__rating d-flex align-items-center gap-5 mb-3">
                                     <div>
                                         <span><i class="ri-star-fill"></i></span>
                                         <span><i class="ri-star-fill"></i></span>
@@ -39,14 +41,74 @@ const ProductDetails = () => {
                                         <span><i class="ri-star-half-line"></i></span>
                                     </div>
 
-                                    <p>({avgRating}ratings)</p>
+                                    <p>(<span>{avgRating}</span>ratings)</p>
                                 </div>
 
-                                <span>{price}</span>
-                                <p>{shortDesc}</p>
+                                <span className="product__price">${price}</span>
+                                <p className="mt-3">{shortDesc}</p>
 
-                                <button className="buy__btn">Add to Cart</button>
+                                <motion.button whileTap={{scale: 1.2}} className="buy__btn">Add to Cart</motion.button>
                             </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="12">
+                            <div className="tab__wrapper d-flex align-items-center gap-5">
+                                <h6 className={`${tab === "desc" ? "active__tab" : ""}`} onClick={() => setTab("desc")}>Description</h6>
+                                <h6 className={`${tab === "rev" ? "active__tab" : ""}`} onClick={() => setTab("rev")}>Reviews ({reviews.length})</h6>
+                            </div>
+                            
+
+                            {
+                                tab === "desc" ? <div className="tab__content mt-5">
+                                <p>{description}</p>
+                            </div> : (
+                                <div className="product__review mt-5">
+                                    <div className="review__wrapper">
+                                        <ul>
+                                            {
+                                                reviews?.map((item, index) => (
+                                                    <li kew={index} className="mb-4">
+                                                        <h6>John doe</h6>
+                                                        <span>{item.rating} (rating)
+                                                        </span>
+                                                        <p>
+                                                        {item.text}
+                                                        </p>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+
+                                        <div className="review__form">
+                                            <h4>Leave your experience</h4>
+                                            <form action="">
+                                                <div className="form__group">
+                                                    <input type="text" placeholder="Enter name"/>
+                                                </div>
+
+                                                <div className="form__group">
+                                                    <span>1<i class="ri-star-s-fill"></i></span>
+                                                    <span>2<i class="ri-star-s-fill"></i></span>
+                                                    <span>3<i class="ri-star-s-fill"></i></span>
+                                                    <span>4<i class="ri-star-s-fill"></i></span>
+                                                    <span>5<i class="ri-star-s-fill"></i></span>
+                                                </div>
+
+                                                <div className="form__group">
+                                                    <input type="text" placeholder="Review Message... "/>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
+                            
                         </Col>
                     </Row>
                 </Container>
