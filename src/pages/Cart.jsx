cb5dc16ac5351b77e79abdd004087d10755cb5c4
datogithub <104518080+datogithub@users.noreply.github.com>
2023-05-26 20:52:28 -0700

    create cart section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,60 @@
 import React from "react";
+import "../styles/cart.css";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+import { Container, Row, Col } from "react-bootstrap";
+
+import tdImg from "../assets/images/arm-chair-01.jpg";
+import { motion } from "framer-motion";
+import { cartAction } from "../redux/slices/cartSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const Cart = () => {
+
+    const cartItems = useSelector(state => state.cart.cartItem);
+
     return (
-        <div>Cart</div>
+        <Helmet title="Cart">
+            <CommonSection title="Shopping Cart" />
+
+            <section>
+                <Container>
+                    <Row>
+                        <Col lg="9">
+                         {
+                            cartItems.length === 0 ? (<h2 className="fs-4 text-center">No item added to the cart</h2>)
+                             : (
+                             <table className="table bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Image</th>
+                                        <th>Title</th>
+                                        <th>Price</th>
+                                        <th>Qty</th>
+                                        <th>Delete</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr>
+                                        <td> <img src={tdImg} alt="" /> </td>
+                                        <td>Modern Arm</td>
+                                        <td>$299</td>
+                                        <td>2px</td>
+                                        <td><i class="ri-delete-bin-line"></i></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            )
+                         }   
+                            
+                        </Col>
+
+                        <Col lg="3"></Col>
+                    </Row>
+                </Container>
+            </section>
+        </Helmet>
     )
 }
 
