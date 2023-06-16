3b3547360c46adabf9041cea0a46dfca208febeb
datogithub <104518080+datogithub@users.noreply.github.com>
2023-06-16 12:01:28 -0700

    create custom-hooks: useAuth, protected route

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -8,19 +8,29 @@ import ProductDetails from "../pages/ProductDetails";
 import Checkout from "../pages/Checkout";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 const Routers = () => {
-    return <Routes>
+    return ( 
+    <Routes>
         <Route path="/" element={<Navigate to="home" />} />
         <Route path="home" element={<Home/>}/>
         <Route path="shop" element={<Shop/>}/>
         <Route path="shop/:id" element={<ProductDetails/>}/>
         <Route path="cart" element={<Cart/>}/>
-        <Route path="checkout" element={<Checkout/>}/>
+        <Route 
+            path="checkout" 
+            element={
+                <ProtectedRoute>
+                     <Checkout />
+                </ProtectedRoute>
+                }
+                />
         <Route path="login" element={<Login/>}/>
         <Route path="signup" element={<Signup/>}/>
     </Routes>
+    )
 }
 
 export default Routers;
